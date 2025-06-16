5883663ec82398a1a57c46e4cd90b77d576c90a3
TimonPahl <timon.pahl@gmail.com>
2025-06-16 14:31:35 +0200

    anpassung mit feinem mesh

--- fcm_code.py
+++ fcm_code.py
@@ -1,11 +1,17 @@
 import mlhp
+import datetime
+import os
+import numpy as np
+import matplotlib.pyplot as plt
 
+##
+time1=datetime.datetime.now()
+print("Start time:", time1)
+print("1. Setting up mesh and basis", flush=True)
 D = 3
 
-print("1. Setting up mesh and basis", flush=True)
-
 # Setup triangulation domain
-triangulation = mlhp.readStl("/Applications/Datein_Timon/Bildung/Uni_Rostock/Master_MaschBau/3_Semester/Studienarbeit/Code/FCM-StA/FCM/Würfel1.stl")
+triangulation = mlhp.readStl("/Applications/Datein_Timon/Bildung/Uni_Rostock/Master_MaschBau/3_Semester/Studienarbeit/Code/FCM-StA/FCM/Wuerfel1.stl")
 kdtree = mlhp.buildKdTree(triangulation)
 domain = mlhp.implicitTriangulation(triangulation, kdtree)
 
@@ -19,10 +25,7 @@ alphaFCM = 1e-5
 penalty = 1e5 * youngsModulus
 
 origin, max = triangulation.boundingBox()
-
-# ##################### FIX ####################
 lengths = [m - o for o, m in zip(origin, max)] 
-
 origin = [o - 1e-10 for o in origin]
 max = [m + 1e-10 for m in max]
 #debugging
@@ -33,21 +36,29 @@ print("  Length :", lengths)
 print("  Fix unten bei z <", origin[2] + lengths[2]*0.01)
 print("  Drück oben bei z >", origin[2] + lengths[2]*0.99)
 
-
-
 lengths = [m - o for o, m in zip(origin, max)]
 
-grid = mlhp.makeRefinedGrid(nelements, lengths, origin)
+grid = mlhp.makeGrid(nelements, lengths, origin) #grobes gitter
+grid = mlhp.makeRefinedGrid(mlhp.makeFilteredGrid(grid, domain=domain, nseedpoints=polynomialDegree + 2))#filterung mit stl-domain
+
 basis = mlhp.makeHpTensorSpace(grid, polynomialDegree, nfields=D)
 
 print(basis)
+time2=datetime.datetime.now()
+print("Time needed: ", time2-time1)
 
+##
+time1=time2
 print("2. Allocating linear system", flush=True)
 
 matrix = mlhp.allocateSparseMatrix(basis)
 vector = mlhp.allocateRhsVector(matrix)
 
-print("2. Computing weak boundary integrals", flush=True)
+time2=datetime.datetime.now()
+print("Time needed: ", time2-time1)
+##
+time1=time2
+print("3. Computing weak boundary integrals", flush=True)
 
 def createBoundaryQuadrature(func):
      filtered = mlhp.filterTriangulation(triangulation, mlhp.implicitFunction(D, func))
@@ -55,12 +66,11 @@ def createBoundaryQuadrature(func):
      quadrature = mlhp.triangulationQuadrature(intersected, celldata,polynomialDegree + 1)
      return intersected, celldata, quadrature
 
-shrink = 0.1
-intersected0, celldata0, quadrature0 = createBoundaryQuadrature(f"x <{origin[0] + shrink*lengths[0]}")
-intersected1, celldata1, quadrature1 = createBoundaryQuadrature(f"x >{origin[0] + lengths[0] - shrink*lengths[0] }") #original value was 1e-2
+intersected0, celldata0, quadrature0 = createBoundaryQuadrature(f"z <{origin[2] + 0.01*lengths[2]}")
+intersected1, celldata1, quadrature1 = createBoundaryQuadrature(f"z >{origin[2] + 0.99*lengths[2] }") 
 
-integrand0 = mlhp.l2BoundaryIntegrand(mlhp.vectorField(D, [penalty] * D), mlhp.vectorField(D, [0.0, 0.0, 0.0]))
-integrand1 = mlhp.l2BoundaryIntegrand(mlhp.vectorField(D, [penalty] * D), mlhp.vectorField(D, [penalty*10.0, 0.0, 0.0])) #defines a Dirichlet BC with displacement in x of 5
+integrand0 = mlhp.l2BoundaryIntegrand(mlhp.vectorField(D, [penalty] * D), mlhp.vectorField(D, [0.0, 0.0, 0.0]))#wird in alle richtungen festgehalten & bestraft
+integrand1 = mlhp.l2BoundaryIntegrand(mlhp.vectorField(D, [penalty] * D), mlhp.vectorField(D, [0, 0.0, -lengths[2]*0.01*penalty])) # verschiebung in z im 1%
 #integrand1 = mlhp.neumannIntegrand(mlhp.vectorField(D, [1e3, 0.0, 0.0]))
 
 
@@ -68,6 +78,11 @@ mlhp.integrateOnSurface(basis, integrand0, [matrix, vector], quadrature0)
 mlhp.integrateOnSurface(basis, integrand1, [matrix, vector], quadrature1)
 #mlhp.integrateOnSurface(basis, integrand1, [vector], quadrature1)
 
+
+time2=datetime.datetime.now()
+print("Time needed: ", time2-time1)
+##
+time1=time2
 print("4. Computing domain integral", flush=True)
 
 E = mlhp.scalarField(D, youngsModulus)
@@ -83,7 +98,11 @@ quadrature = mlhp.momentFittingQuadrature(domain, depth=polynomialDegree, epsilo
 mlhp.integrateOnDomain(basis, integrand, [matrix, vector],
 quadrature=quadrature)
 
-print("6. Solving linear system", flush=True)
+time2=datetime.datetime.now()
+print("Time needed: ", time2-time1)
+##
+time1=time2
+print("5. Solving linear system", flush=True)
 
 #P = mlhp.additiveSchwarzPreconditioner(matrix, basis, dirichlet[0])
 P = mlhp.diagonalPreconditioner(matrix)
@@ -95,7 +114,24 @@ dofs, norms = mlhp.cg(matrix, vector, M=P, maxiter=10000, residualNorms=True)
 #plt.loglog(norms)
 #plt.show()
 
-print("7. Postprocessing solution", flush=True)
+time2=datetime.datetime.now()
+print("Time needed: ", time2-time1)
+##
+time1=time2
+print("6. Postprocessing solution", flush=True)
+
+outputDir="/Applications/Datein_Timon/Bildung/Uni_Rostock/Master_MaschBau/3_Semester/Studienarbeit/Code/FCM-StA/outputsWuerfel1" 
+
+Ku = matrix * dofs
+strainEnergy = 0.0
+for ku, u in zip(Ku, dofs):
+    strainEnergy += ku * u
+strainEnergy = np.sqrt(strainEnergy)
+print("Strain energy: %e" % strainEnergy )
+
+with open(outputDir+"/convergence.csv", "a") as myfile:
+    myfile.write("%d %e\n" % (dofs.size, strainEnergy))
+    myfile.close()
 
 #Output solution on FCM mesh and boundary surface
 gradient = mlhp.projectGradient(basis, dofs, quadrature)
@@ -110,8 +146,6 @@ processors = [mlhp.solutionProcessor(D, dofs, "Displacement"),
 intersected, celldata = mlhp.intersectTriangulationWithMesh(grid, triangulation, kdtree)
 
 surfmesh = mlhp.associatedTrianglesCellMesh(intersected, celldata)
-
-outputDir="/Applications/Datein_Timon/Bildung/Uni_Rostock/Master_MaschBau/3_Semester/Studienarbeit/Code/FCM-StA/FCM/outputsWuerfel1"
 
 writer0 = mlhp.PVtuOutput(filename=outputDir+"/linear_elasticity_fcm_stl_boundary")
 writer1 = mlhp.PVtuOutput(filename=outputDir+"/linear_elasticity_fcm_stl_fcmmesh")
@@ -128,3 +162,7 @@ surfwriter1 = mlhp.VtuOutput(filename=outputDir+"/linear_elasticity_fcm_stl_boun
 
 mlhp.writeMeshOutput(grid, surfmesh0, surfwriter0, [])
 mlhp.writeMeshOutput(grid, surfmesh1, surfwriter1, [])
+
+time2=datetime.datetime.now()
+print("Time needed: ", time2-time1)
+print("Finish time:", time2)
